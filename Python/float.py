56a15760c9beb0a8c97ffbae7140aeea02541cbb
Ahmed Alhadari <ahmedalhadari@gmail.com>
2026-05-14 22:03:32 +0300

    If conditions

---
+++ Python/float.py
@@ -0,0 +1,16 @@
+number1 = 3.15
+number2 = -3.58
+number3 = .5
+number4 = 5.
+number5 = 1_000.5_000_5
+print(f"{number1, number2, number3, number4, number5}")
+
+print(0.48 + 0.59)  # 0.30000000004 (NOT 0.3)
+
+# Solution
+number = 3.25654458
+print(f"{(0.1 + 0.2):.2f}")
+number = round(0.1 + 0.2, 17)
+number = round(1.000001, 6)
+
+print(f"{number}")
---
+++ Python/integers.py
@@ -0,0 +1,7 @@
+number = 42
+number = -16
+number = 0
+number = 1_000_000
+number = 1_0_0_0 
+
+print (f"{number}")
